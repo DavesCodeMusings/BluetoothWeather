7f46d550576ea1fb14c64015545bc9d3cfc2983e
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2020-10-29 18:08:44 -0500

    Create read_humidity.sh

---
+++ read_humidity.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Read humidity from a Bluetooth Low Energy device.
+# The output will be a percentage as specified by the characteristic.
+
+# Default BLE address to use or get from command-line.
+ADDRESS="71:A2:AE:D8:21:30"
+if [ "$1" != "" ]; then
+  ADDRESS=$1
+fi
+
+printf "%d\n" $(gatttool --device=$ADDRESS --char-read --uuid=0x2a6f | awk '{ printf("0x%s%s\n", $5, $4) }') | sed 's/..$/\.&/'
+
+# Explanation of how it works:
+#
+# gatttool --device=$ADDRESS --char-read --uuid=0x2a6f
+#   Read the characteristic for temperature. The output will be come with
+#   extra info that's not needed.
+# awk '{ printf("0x%s%s\n", $5, $4) }')
+#   Extract only the two pairs of hex digits from the output. These are in
+#   LSB first order. Combine them with a leading 0x. For example, 0x0DC1.
+# printf "%d\n"
+#   Take the 0x0DC1 from gatttool | awk, and convert it to a decimal number.
+# sed 's/..$/\.&/'
+#   Take the printf output and insert a decimal point two characters from the
+#   right, because the GATT temperature characteristic has a resolution of two
+#   decimal places.
